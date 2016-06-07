7105b64cbc88c5c62da93c64223eacf381c0a5d4
s15012 <s15012@std.it-college.ac.jp>
2016-06-07 14:25:11 +0900

    Exam

--- algorythm/calc.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-class Cals:
-    def _init_(self):
-        self.STATS = {'INIT':0, 'VAL':1, 'OP':2, 'VAL2':3, 'OK':4, 'NG':5}
-        def isNumeric(self, c):
-            return (c >= '0' and c <= '9')
-
---
+++ tuto/Byte.java
@@ -0,0 +1,13 @@
+public class Byte {
+    public static void main(String...args) {
+
+    int a = 5;
+    byte b = (byte)a;
+
+    System.out.println(Integer.toBinaryString(b));
+
+
+    }
+
+}
+
